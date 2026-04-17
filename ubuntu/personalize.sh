47a98b921879acc6132aa7c9984b4c5631ff3b07
graysmal <graysmal@gmail.com>
2026-04-17 03:26:47 -0500

    appended more settings and cleanup to ubuntu .sh

--- ubuntu/personalize.sh
+++ ubuntu/personalize.sh
@@ -1,58 +1,122 @@
-# IMAGE_URL="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2ac70c0e-3d49-4027-8368-fc728831c239/diy5quc-f98bfe2d-6ea2-4cf2-83db-a41a92086867.png/v1/fill/w_1192,h_670,q_70,strp/_my_little_pony_windows_7_wallpaper_by_mlp123fan_diy5quc-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiIvZi8yYWM3MGMwZS0zZDQ5LTQwMjctODM2OC1mYzcyODgzMWMyMzkvZGl5NXF1Yy1mOThiZmUyZC02ZWEyLTRjZjItODNkYi1hNDFhOTIwODY4NjcucG5nIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.WNytG7eqmrf2WofwqtXVbjSXgNcJuk9cGFfQy4MI7UQ"
-# IMAGE_PATH="$HOME/Pictures/backgrounds/wallpaper.jpg"
+# SCRIPT SETTINGS ==================================
+THEME="rainbowdash" # rainbowdash | fluttershy | cat
+IMAGE_URL="https://tongstonk.com/${THEME}.png"
+IMAGE_PATH="$HOME/Pictures/backgrounds/wallpaper.jpg"
+echo "[i] ${THEME} theme selected."
 
-# echo "[i] sudo apt install curl"
-# sudo apt install -y curl
+# DAMNATIO MEMORIAE SNAP! ==================================
+echo "[i] removing all snap packages..."
+sudo systemctl stop snapd
+sudo systemctl disable snapd
+for snap_pkg in $(snap list | awk 'NR>1 {print $1}'); do
+    echo "    Removing $snap_pkg..."
+    sudo snap remove --purge "$snap_pkg" 2>/dev/null || true
+done
+echo "[+] removed all snap packages."
 
-# echo "[i] downloading wallpaper image from URL"
-# curl --create-dirs -o "$IMAGE_PATH" "$IMAGE_URL"
+echo "[i] purging snap residue from machine..."
+sudo apt purge -y snapd ubuntu-core-launcher squashfs-tools
+sudo rm -rf ~/snap /snap /var/snap /var/lib/snapd /var/cache/snapd
+echo "[i] blacklisting snap for good..."
+echo "Package: snapd
+Pin: release a=*
+Pin-Priority: -1" | sudo tee /etc/apt/preferences.d/nosnap.pref > /dev/null
+echo "[+] snap removed, damnatio memoriae! >:D"
 
-# if [ -f "$IMAGE_PATH" ]; then
-#    echo "[i] wallpaper exists at ~/Pictures/backgrounds/wallpaper.jpg"
-#    echo "[+] setting org.gnome.desktop.background: picture uri, picture-options to image..."
-#    gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_PATH"
-#    gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMAGE_PATH"
-#    gsettings set org.gnome.desktop.background picture-options "zoom"
-# else
-#    echo "[!] wallpaper does not exist at ~/Pictures/backgrounds/wallpaper.jpg"
-# fi
+# installing packages and apps ==================================
+# apt repository --------------------------------
+echo "[i] installing apt packages..."
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl git tree vim gnome-shell-extension-manager
+echo "[+] installed apt packages."
 
-# echo "[+] setting org.gnome.desktop gtk-theme to Yaru-blue..."
-# gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-blue'
-# echo "[+] setting org.gnome.shell.extensions.dask-to-dock position to BOTTOM..."
-# gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-# echo "[+] setting org.gnome.shell.extensions.dask-to-dock extend-height (full-width) to false..."
-# gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
-# echo "[+] setting org.gnome.shell.extensions.dask-to-dock show-trash, show-mounts to false..."
-# gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
-# gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+# external apps (discord, firefox) --------------------------------
+# discord ................................
+echo "[i] downloading discord .deb package..."
+curl -L -o "$HOME/Downloads/discord.deb" "https://discord.com/api/download?platform=linux&format=deb"
+echo "[i] installing discord via .deb package..."
+sudo dpkg -i "$HOME/Downloads/discord.deb" || sudo apt -f install -y
+echo "[+] installed discord."
 
-# echo "[+] setting org.gnome.shell.extensions.ding show-home to false..."
-# gsettings set org.gnome.shell.extensions.ding show-home false
-
-# echo "[+] settings org.gnome.nautilus.preferences show-hidden-files to false..."
-# gsettings set org.gnome.nautilus.preferences show-hidden-files false
-# echo "[i] sudo apt install gnome-shell-extension-manager"
-# sudo apt install -y gnome-shell-extension-manager
+# firefox ................................
+echo "[i] installing firefox via mozilla ppa..."
+sudo add-apt-repository -y ppa:mozillateam/ppa
+echo 'Package: firefox*
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001' | sudo tee /etc/apt/preferences.d/mozilla-firefox > /dev/null
+sudo apt install -y firefox
+echo "[+] installed firefox."
+# TODO: configure firefox?
 
 
+# GSETTINGS ==================================
+# wallpaper --------------------------------
+echo "[i] downloading wallpaper image from URL..."
+curl -L --create-dirs -o "$IMAGE_PATH" "$IMAGE_URL"
+if [ -f "$IMAGE_PATH" ]; then
+    echo "[i] wallpaper exists at ~/Pictures/backgrounds/wallpaper.jpg."
+    echo "[i] modifying gsettings to apply wallpaper..."
+    gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_PATH"
+    gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMAGE_PATH"
+    gsettings set org.gnome.desktop.background picture-options "zoom"
+    echo "[+] wallpaper set."
+else
+    echo "[!] wallpaper does not exist at ~/Pictures/backgrounds/wallpaper.jpg."
+fi
 
+# accent color --------------------------------
+echo "[i] setting gtk-theme to theme preference..."
+case "$THEME" in
+    "rainbowdash")
+        gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-blue'
+        echo "[+] gtk set to Yaru-blue."
+    ;;
+    "fluttershy")
+        gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-magenta'
+        echo "[+] gtk set to Yaru-magenta."
+    ;;
+    "cat")
+        gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-sage'
+        echo "[+] gtk set to Yaru-sage."
+    ;;
+    *)
+        echo "[!] ${THEME} not found as a theme, try rainbowdash, fluttershy, or cat."
+    ;;
+esac
+
+# default settings --------------------------------
+echo "[i] modifying gsettings (dock, keybinds, nautilus, desktop prefs)..."
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+gsettings set org.gnome.shell.extensions.ding show-home false
+gsettings set org.gnome.nautilus.preferences show-hidden-files false
+gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>F4', '<Super>c']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Primary><Alt>t', '<Super>return']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys search "['<Super>space']"
+echo "[+] gsettings changes applied."
+
+# GNOME EXTENSIONS ==================================
+
+# blur-my-shell --------------------------------
 echo "[i] downloading blur-my-shell release v72..."
-# curl -L -o "$HOME/Downloads/blur-my-shell@aunetx.shell-extension.zip" "https://github.com/aunetx/blur-my-shell/releases/download/v72/blur-my-shell@aunetx.shell-extension.zip"
-echo "[+] blur-my-shell installed. relog required"
-# gnome-extensions install "$HOME/Downloads/blur-my-shell@aunetx.shell-extension.zip"
+curl -L -o "$HOME/Downloads/blur-my-shell@aunetx.shell-extension.zip" "https://github.com/aunetx/blur-my-shell/releases/download/v72/blur-my-shell@aunetx.shell-extension.zip"
+echo "[i] installing blur-my-shell."
+gnome-extensions install "$HOME/Downloads/blur-my-shell@aunetx.shell-extension.zip"
+echo "[+] blur-my-shell installed. re-log required for changes?"
 
-echo "[+] adding applications to blur-my-shell"
+echo "[i] modifying dconf blur-my-shell settings..."
 dconf write /org/gnome/shell/extensions/blur-my-shell/applications/blur true 
 dconf write /org/gnome/shell/extensions/blur-my-shell/applications/dynamic-opacity false 
 dconf write /org/gnome/shell/extensions/blur-my-shell/applications/sigma 25
-dconf write /org/gnome/shell/extensions/blur-my-shell/applications/whitelist "['org.gnome.Shell.Extensions', 'com.mattjakeman.ExtensionManager', 'firefox_firefox', 'gnome-terminal-server', 'discord']"
-
-echo "[+] adding default pipeline to blur-my-shell (removing corners effect)"
-dconf write /org/gnome/shell/extensions/blur-my-shell/pipelines "{'pipeline_default': {'name': <'Default'>, 'effects': <[<{'type': <'native_static_gaussian_blur'>, 'id': <'effect_000000000000'>, 'params': <{'radius': <30>, 'brightness': <0.59999999999999998>}>}>]>}, 'pipeline_default_rounded': {'name': <'Default rounded'>, 'effects': <[<{'type': <'native_static_gaussian_blur'>, 'id': <'effect_000000000001'>, 'params': <{'radius': <30>, 'brightness': <0.59999999999999998>}>}>]>}}"
-
-
 dconf write /org/gnome/shell/extensions/blur-my-shell/panel/static-blur false
 dconf write /org/gnome/shell/extensions/blur-my-shell/panel/sigma 25
 dconf write /org/gnome/shell/extensions/blur-my-shell/dash-to-dock/static-blur false
 dconf write /org/gnome/shell/extensions/blur-my-shell/dash-to-dock/sigma 25
+dconf write /org/gnome/shell/extensions/blur-my-shell/pipelines "{'pipeline_default': {'name': <'Default'>, 'effects': <[<{'type': <'native_static_gaussian_blur'>, 'id': <'effect_000000000000'>, 'params': <{'radius': <30>, 'brightness': <0.59999999999999998>}>}>]>}, 'pipeline_default_rounded': {'name': <'Default rounded'>, 'effects': <[<{'type': <'native_static_gaussian_blur'>, 'id': <'effect_000000000001'>, 'params': <{'radius': <30>, 'brightness': <0.59999999999999998>}>}>]>}}"
+dconf write /org/gnome/shell/extensions/blur-my-shell/applications/whitelist "['org.gnome.Shell.Extensions', 'com.mattjakeman.ExtensionManager', 'firefox_firefox', 'gnome-terminal-server', 'discord']"
+echo "[+] dconf blur-my-shell settings applied."
+
